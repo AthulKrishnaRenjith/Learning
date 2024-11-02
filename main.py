9821f8e79e647b8a7b0102814fe40e5c743b4aab
Athul Krishna Renjith <37531495-ar23agl@users.noreply.replit.com>
2024-11-02 15:43:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+
+print (arr)
+
+print ("\n",np.__version__)
+
+print ("\n",type(arr))
+
+ar = np.array((1,23,4,5,6,7))
+
+print ("\n",ar)
+
+ar0 = np.array(45)
+
+print ("\n",ar0)
+
+ar2 = np.array([[1,2,3],[4,5,6]])
+
+print ("\n",ar2)
