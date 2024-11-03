9b9e8e030248b7246028af40f69b08a5d6571f52
AthulKrishnaRenjith <ar23agl@herts.ac.uk>
2024-11-03 18:34:55 +0000

    chng to juptr

--- Projects/rockvmine/tempCodeRunnerFile.py
+++ Projects/rockvmine/tempCodeRunnerFile.py
@@ -4,3 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+sonar_data = pd.read_csv('sonar data.csv', header=None)
+
+sonar_data.head()
